a3eee6ecbdbf8c3afb4ac39cbcc58473679aa611
Joe2k <jonathansamuel2k@gmail.com>
2021-05-06 04:21:04 +0530

    add checks

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('working');
+if (checks()) {
+	alert('Working!');
+}
+
+function checks() {
+	if (location.hostname !== 'psd.bits-pilani.ac.in') {
+		alert('Only works on http://psd.bits-pilani.ac.in');
+		return false;
+	}
+
+	if (!location.pathname.includes('StudentStationPreference.aspx')) {
+		alert('You need to be on Fill Station Prefrence page');
+		return false;
+	}
+
+	if (window.__PSZYSET__ === true) {
+		alert('Already ran here once. Please refresh');
+		return false;
+	}
+
+	return true;
+}
