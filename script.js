d6f6ac87599da85f16fd83fc60dcb0b5dcba5488
Joe2k <jonathansamuel2k@gmail.com>
2021-06-04 23:19:30 +0530

    Update New UI with Domain Selects

--- script.js
+++ script.js
@@ -6,38 +6,115 @@ function createUI() {
 	const mainDiv = $('#rptlist');
 
 	const newDiv = document.createElement('div');
+	newDiv.style.border = '2px solid';
+	newDiv.style.borderColor = '#0275d8';
+	newDiv.style.padding = '1%';
 	newDiv.classList.add('row');
 
-	const textDiv = document.createElement('h5');
-	textDiv.innerText =
-		'Choose the CSV file you want to upload and click Apply Changes Button.';
-	textDiv.classList.add('col-xs-8');
+	const mainHead = document.createElement('h1');
+	mainHead.innerText = 'PS Companion';
+	mainHead.classList.add('text-primary');
+	mainHead.classList.add('col-xs-8');
+	newDiv.appendChild(mainHead);
+
+	const textDiv = document.createElement('div');
+	// textDiv.innerText =
+	// 	'Choose the CSV file you want to upload, select which domains you want on top to rearrange the new PS Stations which are not there in CSV and then click Apply Changes! Button.';
+	textDiv.classList.add('col-xs-12');
+	const step1 = document.createElement('h5');
+	step1.innerText = '1. Choose the CSV file you want to upload.';
+	const step2 = document.createElement('h5');
+	step2.innerText =
+		'2. When a station in the preference list below is not found in the CSV file, then select which all domains stations you want to see on the top. The non selected domains stations will therefore be pushed to the last.';
+	const step3 = document.createElement('h5');
+	step3.innerText = '3. Click on Apply Changes! button.';
+	step1.style.margin = '0px';
+	step2.style.margin = '0px';
+	step3.style.margin = '0px';
+	textDiv.appendChild(step1);
+	textDiv.appendChild(document.createElement('br'));
+	textDiv.appendChild(step2);
+	textDiv.appendChild(document.createElement('br'));
+	textDiv.appendChild(step3);
+	textDiv.style.marginBottom = '2%';
 	newDiv.appendChild(textDiv);
 
 	const input = document.createElement('input');
 	input.type = 'file';
 	input.accept = '.csv, text/csv';
-	input.classList.add('col-xs-2');
+	input.classList.add('col-xs-4');
+	input.style.fontSize = '16px';
 	newDiv.appendChild(input);
+
+	const domainDiv = document.createElement('div');
+	domainDiv.classList.add('col-xs-4');
+	const domainLabel = document.createElement('p');
+	domainLabel.innerText = 'Select Domains';
+	domainLabel.style.fontSize = '16px';
+	const domainSelect = document.createElement('select');
+	domainSelect.classList.add('form-select');
+	domainSelect.setAttribute('multiple', true);
+	const op1 = document.createElement('option');
+	op1.innerText = 'Chemical';
+	const op2 = document.createElement('option');
+	op2.innerText = 'Electronics';
+	const op3 = document.createElement('option');
+	op3.innerText = 'Finance and Mgmt';
+	const op4 = document.createElement('option');
+	op4.innerText = 'Govt Research Lab';
+	const op5 = document.createElement('option');
+	op5.innerText = 'IT';
+	const op6 = document.createElement('option');
+	op6.innerText = 'Infrastructure';
+	const op7 = document.createElement('option');
+	op7.innerText = 'Mechanical';
+	const op8 = document.createElement('option');
+	op8.innerText = 'Health Care';
+	const op9 = document.createElement('option');
+	op9.innerText = 'Others';
+	domainSelect.appendChild(op1);
+	domainSelect.appendChild(op2);
+	domainSelect.appendChild(op3);
+	domainSelect.appendChild(op4);
+	domainSelect.appendChild(op5);
+	domainSelect.appendChild(op6);
+	domainSelect.appendChild(op7);
+	domainSelect.appendChild(op8);
+	domainSelect.appendChild(op9);
+	domainSelect.style.height = '160px';
+	domainSelect.style.width = '100%';
+	domainInfo = document.createElement('p');
+	domainInfo.innerText =
+		'Hold down the Ctrl (windows) or Command (Mac) button to select multiple domains.';
+	domainInfo.classList.add('text-danger');
+	domainDiv.appendChild(domainLabel);
+	domainDiv.appendChild(domainSelect);
+	domainDiv.appendChild(domainInfo);
+	domainDiv.style.marginRight = '10%';
+	newDiv.appendChild(domainDiv);
 
 	const button = document.createElement('button');
 	button.classList.add('btn');
 	button.classList.add('col-xs-2');
 	button.classList.add('btn-primary');
 	button.innerText = 'Apply Changes!';
+	button.style.fontSize = '16px';
 	button.addEventListener('click', function (e) {
 		e.preventDefault();
 		handleClick(input, newDiv);
 	});
-
 	newDiv.appendChild(button);
+
 	const textDiv2 = document.createElement('a');
 	textDiv2.href = 'https://psitseasy.ml/projectBank';
 	textDiv2.innerText =
 		'Click here to download the latest Station Details in CSV from Project Bank at psitseasy.ml';
 	textDiv2.classList.add('col-xs-12');
 	textDiv2.target = '_blank';
+	textDiv2.style.fontSize = '18px';
+	textDiv2.style.marginTop = '2%';
 	newDiv.appendChild(textDiv2);
+
 	newDiv.style.margin = '20px';
 	mainDiv.insertBefore(newDiv, mainDiv.firstChild);
 	window.__PSZYSET__ = true;
